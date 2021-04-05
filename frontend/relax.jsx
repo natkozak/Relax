fc3af2f1554efece00a9cf767c5e6acef1ae1ab3
natkozak <natkozak0@gmail.com>
2021-04-05 13:13:22 -0700

    Generate package.json, install npm dependencies, create entry file

---
+++ frontend/relax.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  const hello = <div>Hello, world! From, Relax.</div> 
+  ReactDOM.render(hello, root);
+});
