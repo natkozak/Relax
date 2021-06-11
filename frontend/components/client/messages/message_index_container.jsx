1ea13af05eb2a974fc4920f70c040e20c2037329
natkozak <natkozak0@gmail.com>
2021-06-10 17:46:56 -0700

    Refactor message-index-container

--- frontend/components/client/messages/message_index_container.jsx
+++ frontend/components/client/messages/message_index_container.jsx
@@ -1,11 +1,4 @@
 import { connect } from 'react-redux';
-import { 
-  requestMessages, 
-  receiveMessage, 
-  removeMessage, 
-  receiveComment,
-  removeComment 
-} from '/frontend/actions/message_actions';
 import MessageIndex from './message_index';
 
 const mapSTP = state => ({
@@ -13,12 +6,4 @@ const mapSTP = state => ({
   currentUser: state.session.id,
 });
 
-const mapDTP = dispatch => ({
-  requestMessages: () => dispatch(requestMessages()),
-  receiveMessage: (message) => dispatch(receiveMessage(message)),
-  removeMessage: (messageId) => dispatch(removeMessage(messageId)),
-  receiveComment: (comment) => dispatch(receiveComment(comment)),
-  removeComment: (commentId) => dispatch(removeComment(commentId)),
-});
-
-export default connect(mapSTP, mapDTP)(MessageIndex);
+export default connect(mapSTP, null)(MessageIndex);
