927f4711929efe78d3172cdc16b69ce2fd7c9e60
natkozak <natkozak0@gmail.com>
2021-08-06 20:45:54 -0700

    Make links open in new tabs

--- frontend/components/splash/splash_nav.jsx
+++ frontend/components/splash/splash_nav.jsx
@@ -10,9 +10,9 @@ class SplashNav extends React.Component {
     return (
       <div className="splash-nav-div">
         <Link className="logo-header" to='/'><i className="far fa-sun"></i><b>Relax</b></Link>
-        <a className="header-link" href="https://github.com/natkozak/Relax">Project</a>
-        <a className="header-link" href="https://github.com/natkozak">Github</a>
-        <a className="header-link" href="https://www.linkedin.com/in/nat-kozak-23179049/">LinkedIn</a>
+        <div className="header-link" onClick={() => window.open("https://github.com/natkozak/Relax")}>Project</div>
+        <div className="header-link" onClick={() => window.open("https://github.com/natkozak")}>Github</div>
+        <div className="header-link" onClick={() => window.open("https://www.linkedin.com/in/nat-kozak-23179049/")}>LinkedIn</div>
         <Link className="header-link" to='/signin' className="header-link">Sign in</Link>
         <Link to='/createnew' className='splash-try-button'>TRY FOR FREE</Link>
       </div>
